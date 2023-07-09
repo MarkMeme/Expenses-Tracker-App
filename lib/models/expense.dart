dbc3d33f27a59a95d7ba0dd110d5979069e1943b
MarkMeme <markmeme04@gmail.com>
2023-07-09 22:23:19 +0300

    charts and some edits in theming

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -33,3 +33,24 @@ class Expense {
     return DateFormat.yMEd().format(date);
   }
 }
+
+class ExpenseBucket {
+  final Category category;
+  List<Expense> expenses;
+
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  double get totalAmounts {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
