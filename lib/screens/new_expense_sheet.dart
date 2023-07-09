3db4cff77c320ae33034fab90457023f686faf27
MarkMeme <markmeme04@gmail.com>
2023-07-09 13:24:24 +0300

    bottom sheet of adding new expense

--- lib/screens/new_expense_sheet.dart
+++ lib/screens/new_expense_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:expense_tracker_app/models/expense.dart';
 
 class NewExpenseSheet extends StatefulWidget {
   @override
@@ -6,19 +8,122 @@ class NewExpenseSheet extends StatefulWidget {
 }
 
 class _NewExpenseSheetState extends State<NewExpenseSheet> {
+  final _titleContoller = TextEditingController();
+  final _amountContoller = TextEditingController();
+  var _selectedDate = DateTime.now();
+  Category _selectedCategory = Category.leisure;
+
+  var _formKey = GlobalKey<FormState>();
+
+  void _presenteDate() async {
+    var now = DateTime.now();
+    var firstDate = DateTime(now.year - 1, now.month, now.day);
+    var packedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+
+    setState(() {
+      _selectedDate = packedDate!;
+    });
+  }
+
+  void submitForm() {
+    if (_formKey.currentState!.validate() == true) {
+      print("done !");
+    }
+  }
+
+  @override
+  void dispose() {
+    _titleContoller.dispose();
+    _amountContoller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15),
       child: Form(
+          key: _formKey,
           child: Column(
-        children: [
-          TextFormField(
-            maxLength: 20,
-            decoration: const InputDecoration(hintText: 'Expense Title'),
-          )
-        ],
-      )),
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the Expense Title ';
+                  }
+                  return null;
+                },
+                controller: _titleContoller,
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text('Expense Title')),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter the amount';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      controller: _amountContoller,
+                      maxLength: 20,
+                      decoration: const InputDecoration(
+                          prefix: Text('\$ '), label: Text('Expense Amount')),
+                    ),
+                  ),
+                  Expanded(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                          onPressed: _presenteDate,
+                          icon: const Icon(Icons.calendar_view_month_outlined)),
+                      Text(DateFormat.yMEd().format(_selectedDate))
+                    ],
+                  ))
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  DropdownButton(
+                      value: _selectedCategory,
+                      items: Category.values
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value.name.toUpperCase()),
+                              ))
+                          .toList(),
+                      onChanged: (category) {
+                        if (category == null) {
+                          return;
+                        }
+                        setState(() {
+                          _selectedCategory = category;
+                        });
+                      }),
+                  const Spacer(),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel')),
+                  ElevatedButton(
+                      onPressed: submitForm,
+                      child: const Text('Save the title')),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
